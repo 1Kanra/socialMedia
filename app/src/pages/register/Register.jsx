7bfa12fcf7d0f6f6538ca01c2852fc369804d387
1Kanra <notfromouterspace.turtle@gmail.com>
2024-12-25 17:32:38 +0800

    added components

--- app/src/pages/register/Register.jsx
+++ app/src/pages/register/Register.jsx
@@ -1,9 +1,35 @@
-import './register.scss'
+import { Link } from "react-router-dom";
+import "./register.scss";
 
-const Register = () => {
+const Login = () => {
   return (
-    <div className='register'>Register</div>
-  )
-}
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Join the Club</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            posuere, erat elementum tincidunt elementum, sapien nisi vehicula
+            urna, sit amet consequat orci nibh sed ex.
+          </p>
+          <span>Don't have an account?</span>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Email" />
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Confirm Password" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Register
+export default Login;
